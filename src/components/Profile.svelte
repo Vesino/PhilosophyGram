877956f412c49088de5b0ee73d1a0ef15d495238
Luis Vargas <luisvargasr10@gmail.com>
2022-01-16 22:24:29 -0600

    Creation of main components

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://randomuser.me/api/portraits/men/31.jpg" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>Vesino</h2>
+            <span>Vesino Vargs</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,17 @@
+<div class="Stories">
+    <div class="Stories-container">
+        <div class="Stories-head">
+            <h2>Historias</h2>
+            <span>Todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="">
+                </div>
+                <h2>Vesino</h2>
+                <span>10 horas antes</span>
+            </div>
+        </div>
+    </div>
+</div>
